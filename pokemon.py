3dfeba85e38fd15fdc9c00e71bc783bd3dc3dd8c
diyarao-22 <drao2@cps.edu>
2026-05-28 12:17:21 -0500

    Add files via upload

---
+++ pokemon.py
@@ -0,0 +1,203 @@
+#Diya
+#pokemons
+
+#init
+level = 0
+name = "squirtle"
+day = 1
+import random
+#function
+def squirtle():
+    print((r"              _,........__\n"))
+    print((r"           ,-'            \"`-.\n"))
+    print((r"         ,'                   `-.\n"))
+    print((r"       ,'                        \\\n"))
+    print((r"     ,'                           .\n"))
+    print((r"     .'\\               ,\"\".       `\n"))
+    print((r"    ._.'|             / |  `       \\\n"))
+    print((r"    |   |            `-.'  ||       `.\n"))
+    print((r"    |   |            '-._,'||       | \\\n"))
+    print((r"    .`.,'             `..,'.'       , |`-.\n"))
+    print((r"    l                       .'`.  _/  |   `.\n"))
+    print((r"    `-.._'-   ,          _ _'   -\" \\  .     `\n"))
+    print(("`.\"\"\"\"\"'-.`-...,---------','         `. `....__.\n"))
+    print((".'        `\"-..___      __,'\\          \\  \\     \\\n"))
+    print(("\\_ .          |   `\"\"\"\"'    `.           . \\     \\\n"))
+    print((r" `.          |              `.          |  .     L\n"))
+    print((r"   `.        |`--...________.'.        j   |     |\n"))
+    print((r"     `._    .'      |          `.     .|   ,     |\n"))
+    print((r"        `--,\\       .            `7\"\"' |  ,      |\n"))
+    print((r"           ` `      `            /     |  |      |    _,-'\"\"\"`-.\n"))
+    print((r"            \\ `.     .          /      |  '      |  ,'          `.\n"))
+    print((r"             \\  v.__  .        '       .   \\    /| /              \\\n"))
+    print((r"              \\/    `\"\"\\\"\"\"\"\"\"\"`.       \\   \\  /.''                |\n"))
+    print((r"               `        .        `._ ___,j.  `/ .-       ,---.     |\n"))
+    print((r"               ,`-.      \\         .\"     `.  |/        j     `    |\n"))
+    print((r"              /    `.     \\       /         \\ /         |     /    j\n"))
+    print((r"             |       `-.   7-.._ .          |\"          '         /\n"))
+    print((r"             |          `./_    `|          |            .     _,'\n"))
+    print((r"             `.           / `----|          |-............`---'\n"))
+    print((r"               \\          \\      |          |\n"))
+    print((r"              ,'           )     `.         |\n"))
+    print((r"               7____,,..--'      /          |\n"))
+    print((r"                                 `---.__,--.'mh\n"))
+def wartortle():
+    print((r"    __                                _.--'\"7\n"))
+    print((r"   `. `--._                        ,-'_,-  ,'\n"))
+    print((r"    ,'  `-.`-.                   /' .'    ,|\n"))
+    print((r"    `.     `. `-     __...___   /  /     - j\n"))
+    print((r"      `.     `  `.-\"\"        \" .  /       /\n"))
+    print((r"        \\     /                ` /       /\n"))
+    print((r"         \\   /                         ,'\n"))
+    print((r"         '._'_               ,-'       |\n"))
+    print((r"            | \\            ,|  |   ...-'\n"))
+    print((r"            || `         ,|_|  |   | `             _..__\n"))
+    print((r"           /|| |          | |  |   |  \\  _,_    .-\"     `-.\n"))
+    print((r"          | '.-'          |_|_,' __!  | /|  |  /           \\\n"))
+    print((r"  ,-...___ .=                  ._..'  /`.| ,`,.      _,.._ |\n"))
+    print((r" |   |,.. \\     '  `'        ____,  ,' `--','  |    /      |\n"))
+    print((r",`-..'  _)  .`-..___,---'_...._/  .'      '-...'   |      /\n"))
+    print(("'.__' \"\"'      `.,------'\"'      ,/            ,     `.._.' `.\n"))
+    print((r" `.             | `--........,-'.            .         \\     \\\n"))
+    print((r"   `-.          .   '.,--\"\"     |           ,'\\        |      .\n"))
+    print((r"      `.       /     |          L          ,\\  .       |  .,---.\n"))
+    print((r"        `._   '      |           \\        /  .  L      | /   __ `.\n"))
+    print((r"           `-.       |            `._   ,    l   .    j |   '  `. .\n"))
+    print((r"             |       |               `\"' |  .    |   /  '      .' |\n"))
+    print((r"             |       |                   j  |    |  /  , `.__,'   |\n"))
+    print((r"             `.      L                 _.   `    j ,'-'           |\n"))
+    print((r"              |`\"---..\\._______,...,--' |   |   /|'      /        j\n"))
+    print((r"              '       |                 |   .  / |      '        /\n"))
+    print((r"               .      .              ____L   \\'  j    -',       /\n"))
+    print((r"              / `.     .          _,\"     \\   | /  ,-','      ,'\n"))
+    print((r"             /    `.  ,'`-._     /         \\  i'.,'_,'      .'\n"))
+    print((r"            .       `.      `-..'             |_,-'      _.'\n"))
+    print((r"            |         `._      |            ''/      _,-'\n"))
+    print((r"            |            '-..._\\             `__,.--'\n"))
+    print((r"           ,'           ,' `-.._`.            .\n"))
+    print((r"          `.    __      |       \"'`.          |\n"))
+    print((r"            `-\"'  `\"\"\"\"'            7         `.\n"))
+    print((r"                                   `---'--.,'\"`' mh\n"))
+def blastoise():
+    print((r"                      _\n"))
+    print((r"           _,..-\"\"\"--' `,.-\".\n"))
+    print((r"         ,'      __.. --',  |\n"))
+    print((r"       _/   _.-\"' |    .' | |       ____\n"))
+    print((r" ,.-\"\"'    `-\"+.._|     `.' | `-..,',--.`.\n"))
+    print((r"|   ,.                      '    j 7    l \\__\n"))
+    print((r"|.-'                            /| |    j||  .\n"))
+    print((r"`.                   |         / L`.`\"\"','|\\  \\\n"))
+    print((r"  `.,----..._       ,'`\"'-.  ,'   \\ `\"\"'  | |  l\n"))
+    print((r"    Y        `-----'       v'    ,'`,.__..' |   .\n"))
+    print((r"     `.                   /     /   /     `.|   |\n"))
+    print((r"       `.                /     l   j       ,^.  |L\n"))
+    print((r"         `._            L       +. |._   .' \\|  | \\\n"))
+    print((r"           .`--...__,..-'\"\"'-._  l L  \"\"\"    |  |  \\\n"))
+    print((r"         .'  ,`-......L_       \\  \\ \\     _.'  ,'.  l\n"))
+    print((r"      ,-\"`. / ,-.---.'  `.      \\  L..--\"'  _.-^.|   l\n"))
+    print((r".-\"\".'\"`.  Y  `._'   '    `.     | | _,.--'\"     |   |\n"))
+    print((r" `._'   |  |,-'|      l     `.   | |\"..          |   l\n"))
+    print((r" ,'.    |  |`._'      |      `.  | |_,...---\"\"\"\"\"`    L\n"))
+    print((r"/   |   j _|-' `.     L       | j ,|              |   |\n"))
+    print(("`--,\"._,-+' /`---^..../._____,.L',' `.             |\\  |\n"))
+    print((r"  |,'      L                   |     `-.          | \\j\n"))
+    print((r"           .                    \\       `,        |  |\n"))
+    print((r"            \\                __`.Y._      -.     j   |\n"))
+    print((r"             \\           _.,'       `._     \\    |  j\n"))
+    print((r"             ,-\"`-----\"\"\"\"'           |`.    \\  7   |\n"))
+    print((r"            /  `.        '            |  \\    \\ /   |\n"))
+    print((r"           |     `      /             |   \\    Y    |\n"))
+    print((r"           |      \\    .             ,'    |   L_.-')\n"))
+    print((r"            L      `.  |            /      ]     _.-^._\n"))
+    print((r"             \\   ,'  `-7         ,-'      / |  ,'      `-._\n"))
+    print((r"            _,`._       `.   _,-'        ,',^.-            `.\n"))
+    print((r"         ,-'     v....  _.`\"',          _:'--....._______,.-'\n"))
+    print((r"       ._______./     /',,-'\"'`'--.  ,-'  `.\n"))
+    print((r"                \"\"\"\"\"`.,'         _\\`----...' mh\n"))
+    print((r"                       --------\"\"'\n"))
+    print(("\n"))
+    print(("\n"))
+def choice():
+    global level
+    global name
+    global day
+    while True:
+        print(f"it is day {day}")
+        choice = input("do you want to rest,train, gym battle, or final boss battle today?: ")
+        day = day + 1
+        if choice == "rest":
+            print(f"{name} did not train,{name} is now level {level}")
+            print(level)
+            squirtle()
+            continue
+        elif choice == "train":
+             training()
+             print(f"it is now day {day}")
+             continue
+        elif choice == "gym battle":
+            gymbattle()
+            continue
+        elif choice == "final boss battle":
+            finalbossbattle()
+            break
+        else:
+            print("please enter the correct input")
+
+def training():
+    global level
+    global name
+    global day
+    level = level + 1
+    print(f"{name} trains. {name} is now level {level}")
+    print(level)
+    evolve()
+
+def gymbattle():
+    print("you enter a friendly battle with a friend. If you win, +2 levels. If you lose, nothing happens")
+    outcome = random.randint(1,2)
+    if outcome == 1:
+        print("you win! +2 levels")
+        level = level + 2
+        print (f"{name} is now level {level}")
+        print{level}
+    elif outcome == 2:
+        print(f"you lose! Sadness! {name} is now level {level}")
+        print{level}
+
+def finalbossbattle():
+    print("you are now battling the FINAL BOSS. If you lose, GAME OVER")
+    if level < 10:
+        outcome = random.randint(1,4)
+        if outcome == 1:
+            print("You Win!! Yay go celebrate! Game over!")
+        else:
+            print("YOU LOSE! Sadness! Game over!")
+    elif 10<= level < 20:
+        outcome = random.randint(1,3)
+        if outcome == 1:
+            print ("YOU WIN, YAYYYYY! celebrate cause games over")
+        else:
+            print("YOU LOSE, sorry:( Game over")
+
+
+def evolve():
+    if level == 10:
+        name = "wartortle"
+        print(f" You have reached level {level}!, You have now evolved into {name}!")
+        wartortle()
+    elif level == 20:
+        name = "blastoise"
+        print(f" you have reached level {level}!, you have now evolved into {name}")
+        blastoise()
+
+
+
+
+
+def main():
+    squirtle()
+    choice()
+
+
+main()
+
